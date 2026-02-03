e2c1ee04c7fccaf637590ddbe533b847d50c0b2f
Lucas <lucas.laulanet08@gmail.com>
2026-02-03 11:08:37 +0100

    initialize repo

---
+++ plugins/glpiinventory/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.5.6',
+        'version' => '1.5.6.0',
+        'reference' => '15ff053b10f95dcf930bf78eb05c06f512569960',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.5.6',
+            'version' => '1.5.6.0',
+            'reference' => '15ff053b10f95dcf930bf78eb05c06f512569960',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
